72d7bbd33850ce765438743b4742a3d07e9db3d4
jcdenton23 <fifaxjcd23@gmail.com>
2021-11-06 15:27:27 +0600

    feat: copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const { join } = require('path');
+const { readdir, copyFile, mkdir, rmdir } = require('fs/promises');
 
+const mainPath = join(__dirname, 'files');
+const copyPath = join(__dirname, 'files-copy');
+
+async function copyDirectory(mainLink, copyLink) {
+  await rmdir(copyLink, { recursive: true });
+  await mkdir(copyLink, { recursive: true });
+
+  try {
+    const files = await readdir(mainLink, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        await copyFile(join(mainLink, file.name), join(copyLink, file.name));
+      } else
+        copyDirectory(join(mainLink, file.name), join(copyLink, file.name));
+    }
+    process.stdout.write('Все файлы скопированы.\n');
+  } catch {
+    process.stdout.write('Произошла ошибка.\n');
+  }
+}
+
+copyDirectory(mainPath, copyPath);
